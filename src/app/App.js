d9d77f8b3870a7e149b6279ae723d0071f9a5295
Nagaraju <nagaraju.b22@outlook.com>
2021-07-10 22:42:46 +0530

    iload bowlpin model

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import { Canvas } from '@react-three/fiber';
+import Bowlpin from '../components/bowlpin/bowlpin';
 import GizmoHelperComp from '../helpers/dev/GizmoHelper';
 import './App.css';
 
@@ -6,11 +7,10 @@ export default function App() {
     return(
         <Canvas>
             <GizmoHelperComp/>
-
-            <mesh>
-                <boxBufferGeometry/>
-                <meshStandardMaterial/>
-            </mesh>
+            <Bowlpin/>
+            <gridHelper/>
+            <ambientLight intensity={0.1} />
+            <directionalLight args={['#aaaaaa']} position={[10, 10, 10]} castShadow="true" />
         </Canvas>
     )
 }
